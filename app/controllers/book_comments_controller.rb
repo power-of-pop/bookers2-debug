6636d8e7a5938a6468c89d64e75991d4d0750a8c
EC2 Default User <ec2-user@ip-172-31-39-216.ap-northeast-1.compute.internal>
2025-01-02 08:05:14 +0000

    [Update] comment

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,12 +5,13 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_to book_path(book)
+    
   end
 
   def destroy
-    BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    book_comment = BookComment.find(params[:id]).destroy
+    book_comment.destroy
+    book = Book.find(params[:book_id])
   end
 
   private
